66721387d30b1068a9042c946cef70b67a6bbbbe
keigrey <keixgrey@gmail.com>
2022-08-22 21:25:00 +0900

    feat: Add GET /entry/ endpoint

--- controller/entry.js
+++ controller/entry.js
@@ -7,4 +7,26 @@ module.exports = {
     console.log(entryData);
     res.status(201).send("DEKITA");
   },
+
+  async getAllEntries(req, res) {
+    const list = [
+      {
+        date: "2022-08-22",
+        title: "Sping Water",
+        text: "Gratitude turns what we have into enough.",
+      },
+      {
+        date: "2016-01-06",
+        title: "Autumn Leaves",
+        text: "Joy is the simplest form of gratitude.",
+      },
+      {
+        date: "2036-10-15",
+        title: "Winter Wind",
+        text: "Gratitude is not only the greatest of virtues but the parent of all others.",
+      },
+    ];
+
+    res.status(200).json(list);
+  },
 };
--- routes/entry.js
+++ routes/entry.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const entryController = require("../controller/entry");
 
 router.post("/", entryController.addEntry);
+router.get("/", entryController.getAllEntries);
 
 module.exports = router;
