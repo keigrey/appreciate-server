e51026e6672b9b9f64cc4722ca4e13216c24a1fc
keigrey <keixgrey@gmail.com>
2022-08-22 10:49:06 +0900

    chore: Setup dependencies

---
+++ db/knexfile.js
@@ -0,0 +1,32 @@
+// Update with your config settings.
+require("dotenv").config({ path: "../.env" });
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+  development: {
+    client: "pg",
+    connection: {
+      database: process.env.DB_NAME,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+    },
+    migrations: {
+      directory: "./migrations",
+    },
+    seeds: { directory: "./seeds" },
+  },
+
+  production: {
+    client: "pg",
+    connection: {
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false },
+    },
+    migrations: {
+      directory: "./migrations",
+    },
+    seeds: { directory: "./seeds" },
+  },
+};
