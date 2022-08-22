d61d89cbeab574bdeb5fbcedc3042794776a9500
keigrey <keixgrey@gmail.com>
2022-08-22 22:16:30 +0900

    feat: Add id property to local list variable

--- controller/entry.js
+++ controller/entry.js
@@ -11,16 +11,19 @@ module.exports = {
   async getAllEntries(req, res) {
     const list = [
       {
+        id: 3,
         date: "2022-08-22",
         title: "Sping Water",
         text: "Gratitude turns what we have into enough.",
       },
       {
+        id: 4,
         date: "2016-01-06",
         title: "Autumn Leaves",
         text: "Joy is the simplest form of gratitude.",
       },
       {
+        id: 5,
         date: "2036-10-15",
         title: "Winter Wind",
         text: "Gratitude is not only the greatest of virtues but the parent of all others.",
