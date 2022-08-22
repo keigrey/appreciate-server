2b58f28d4a4761a7860e4dd8b5cb2b4e7ea845ce
keigrey <keixgrey@gmail.com>
2022-08-22 11:21:06 +0900

    feat: Create database migrations

---
+++ db/migrations/20220822021442_entry.js
@@ -0,0 +1,26 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable("entry", function (table) {
+    table.increments("id").primary();
+    table
+      .integer("user_id", 32)
+      .notNullable()
+      .references("id")
+      .inTable("user")
+      .onDelete("CASCADE");
+    table.string("title", 72).notNullable();
+    table.string("text", 500).notNullable();
+    table.timestamps(true, true);
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTable("entry");
+};
