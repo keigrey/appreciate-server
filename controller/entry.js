cc3c653fc53c73350b7285306516e7b165efda9a
keigrey <keixgrey@gmail.com>
2022-08-23 13:22:13 +0900

    feat: Create a function to authenticate given token

--- controller/entry.js
+++ controller/entry.js
@@ -1,14 +1,43 @@
 const entryModel = require("../model/entry");
+const jwt = require("jsonwebtoken");
 
 module.exports = {
+  authenticateToken(req, res, next) {
+    const authHeader = req.headers["authorization"];
+    const token = authHeader && authHeader.split(" ")[1];
+
+    if (token === null) {
+      return res.status(401).send();
+    }
+
+    jwt.verify(
+      token,
+      process.env.ACCESS_TOKEN_SECRET,
+      (error, decodedToken) => {
+        if (error) {
+          return res.status(403).send("BAD TOKEN K");
+        }
+        req.body = { id: decodedToken.id };
+
+        next();
+      }
+    );
+  },
+
   async addEntry(req, res) {
     const entryData = await req.body;
     // await entryModel.addEntry(entryData);
-    console.log(entryData);
-    res.status(201).send("DEKITA");
+    // console.log("📗📗📗📗📗📗📗📗📗📗");
+    // console.log(entryData);
+    res.status(201).send(`DEKITA ID: ${entryData.id}`);
   },
 
   async getAllEntries(req, res) {
+    const entryData = await req.body;
+    // await entryModel.addEntry(entryData);
+    console.log("📗📗📗📗📗📗📗📗📗📗");
+    console.log(entryData);
+
     const list = [
       {
         id: 3,
