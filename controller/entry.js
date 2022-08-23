81fdbede40f4dfc93b354246955c7b2b00b00d27
keigrey <keixgrey@gmail.com>
2022-08-23 13:48:11 +0900

    feat: Get all entries from database

--- controller/entry.js
+++ controller/entry.js
@@ -34,31 +34,30 @@ module.exports = {
 
   async getAllEntries(req, res) {
     const entryData = await req.body;
-    // await entryModel.addEntry(entryData);
-    console.log("📗📗📗📗📗📗📗📗📗📗");
-    console.log(entryData);
 
-    const list = [
-      {
-        id: 3,
-        date: "2022-08-22",
-        title: "Sping Water",
-        text: "Gratitude turns what we have into enough.",
-      },
-      {
-        id: 4,
-        date: "2016-01-06",
-        title: "Autumn Leaves",
-        text: "Joy is the simplest form of gratitude.",
-      },
-      {
-        id: 5,
-        date: "2036-10-15",
-        title: "Winter Wind",
-        text: "Gratitude is not only the greatest of virtues but the parent of all others.",
-      },
-    ];
+    const userEntries = await entryModel.getAllUserEntries(entryData);
 
-    res.status(200).json(list);
+    res.status(200).json(userEntries);
+
+    // const list = [
+    //   {
+    //     id: 3,
+    //     date: "2022-08-22",
+    //     title: "Sping Water",
+    //     text: "Gratitude turns what we have into enough.",
+    //   },
+    //   {
+    //     id: 4,
+    //     date: "2016-01-06",
+    //     title: "Autumn Leaves",
+    //     text: "Joy is the simplest form of gratitude.",
+    //   },
+    //   {
+    //     id: 5,
+    //     date: "2036-10-15",
+    //     title: "Winter Wind",
+    //     text: "Gratitude is not only the greatest of virtues but the parent of all others.",
+    //   },
+    // ];
   },
 };
--- model/entry.js
+++ model/entry.js
@@ -4,4 +4,13 @@ const TABLE_NAME = "entry";
 
 module.exports = {
   TABLE_NAME,
+
+  getAllUserEntries({ id }) {
+    console.log("🐽🐽🐽🐽🐽🐽🐽");
+    console.log(id);
+    return knex
+      .select("id", "title", "text", "date")
+      .from(TABLE_NAME)
+      .where({ user_id: id });
+  },
 };
