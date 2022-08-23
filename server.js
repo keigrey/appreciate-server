ce4d6f67d58350f68e7a052caa62d5ec47744884
keigrey <keixgrey@gmail.com>
2022-08-23 17:02:33 +0900

    fix: Assign PORT based on environment

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const PORT = 8080;
+
+const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(express.static("public"));
